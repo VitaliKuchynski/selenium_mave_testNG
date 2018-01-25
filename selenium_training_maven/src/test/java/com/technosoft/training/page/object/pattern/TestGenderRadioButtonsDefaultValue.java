ef992c111fe47cf7641cd8918704c5aeebe02812
Vitali Kuchynski <vitali.kuchynski@gmail.com>
2018-01-25 08:19:52 -0500

    page-object-pattern

---
+++ selenium_training_maven/src/test/java/com/technosoft/training/page/object/pattern/TestGenderRadioButtonsDefaultValue.java
@@ -0,0 +1,28 @@
+package com.technosoft.training.page.object.pattern;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestGenderRadioButtonsDefaultValue extends  DriverWrapper{
+
+
+    @Test
+    //Verifies that male radio-button unchecked by default
+    public void verifyMailRadioButtonDefault(){
+        CreateNewAccountPage createNewAccountPage = new CreateNewAccountPage();
+        LogInPage logInPage = new LogInPage();
+        logInPage.clickOnSignUpLink();
+        Assert.assertFalse( createNewAccountPage.isMailRadioButtonSelected());
+    }
+
+    @Test
+    //Verifies that female radio-button unchecked by default
+    public void verifyFemaleRadioButtonDefault(){
+            CreateNewAccountPage createNewAccountPage = new CreateNewAccountPage();
+            LogInPage logInPage = new LogInPage();
+            logInPage.clickOnSignUpLink();
+            Assert.assertFalse( createNewAccountPage.isFemaleRadioButtonSelected());
+        }
+
+    }
+
