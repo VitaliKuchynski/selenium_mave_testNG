bbde844fc9f01bad794454fe2ada72bb965f4c83
Vitali Kuchynski <vitali.kuchynski@gmail.com>
2018-01-26 00:09:10 -0500

    verify gender error message

--- selenium_training_maven/src/test/java/com/technosoft/training/page/object/pattern/TestRegistrationWithoutGender.java
+++ selenium_training_maven/src/test/java/com/technosoft/training/page/object/pattern/TestRegistrationWithoutGender.java
@@ -1,6 +1,8 @@
 package com.technosoft.training.page.object.pattern;
 
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestRegistrationWithoutGender extends DriverWrapper{
@@ -15,6 +17,8 @@ public class TestRegistrationWithoutGender extends DriverWrapper{
         createNewAccountPage.createNewUserAccount("Semen","Semenov","9685948903","qwe321Q");
         createNewAccountPage.selectBirthday("Jun","1","1984");
         createNewAccountPage.clickOnCreateButton();
-
+        String expectedResult = getDriver().findElement(By.xpath("//*[@class=\"uiContextualLayer uiContextualLayerLeft\"]/descendant::div[2]")).getText();
+        //Verify that missing gender error displays
+        Assert.assertEquals(expectedResult,"Please choose a gender. You can change who can see this later.");
     }
 }
